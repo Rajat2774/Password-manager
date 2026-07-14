9ad32635fb7483a7029d59e2cc5d4d5dda31ca4c
Rajat Singh <rajatsingh2774@gmail.com>
2026-07-14 12:39:52 +0530

    removed the extension folder

--- Lockora/lockora-extension/firebase-config.js
+++
@@ -1,5 +0,0 @@
-// lockora-extension/firebase-config.js
-// Re-exports credentials from the gitignored secrets.js file.
-// See secrets.example.js for the template.
-
-export { firebaseConfig, GOOGLE_CLIENT_ID } from "./secrets.js";
